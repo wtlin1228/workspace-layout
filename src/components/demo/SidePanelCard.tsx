cf3d985d85bb6c67cd5ae3364205a27b6fb07a66
wtlin1228 <wtlin1228@gmail.com>
2025-08-21 15:27:20 +0900

    add side panel card

---
+++ src/components/demo/SidePanelCard.tsx
@@ -0,0 +1,84 @@
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardContent from "@mui/material/CardContent";
+import IconButton from "@mui/material/IconButton";
+import AddIcon from "@mui/icons-material/Add";
+import PestControlRodentIcon from "@mui/icons-material/PestControlRodent";
+import Divider from "@mui/material/Divider";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import WifiTetheringOffIcon from "@mui/icons-material/WifiTetheringOff";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemIcon from "@mui/material/ListItemIcon";
+
+export function SidePanelCardDemo() {
+  return (
+    <Card sx={{ borderRadius: "10px" }} elevation={12}>
+      <CardHeader
+        avatar={
+          <PestControlRodentIcon
+            sx={(theme) => ({ color: theme.palette.grey[300] })}
+          />
+        }
+        action={
+          <IconButton aria-label="add" color="primary">
+            <AddIcon />
+          </IconButton>
+        }
+        title="Tom & Jerry"
+        subheader="Tom and associated Jerrys"
+      />
+      <Divider />
+      <CardContent>
+        <List>
+          <ListItem>
+            <ListItemIcon>
+              <CheckCircleIcon color="success" />
+            </ListItemIcon>
+            <ListItemText
+              slotProps={{ primary: { variant: "body2" } }}
+              primary="Toodles Galore"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemIcon>
+              <CheckCircleIcon color="success" />
+            </ListItemIcon>
+            <ListItemText
+              slotProps={{ primary: { variant: "body2" } }}
+              primary="Jeannie and the Baby"
+            />
+          </ListItem>
+        </List>
+
+        <Divider />
+
+        <List>
+          <ListItem>
+            <ListItemIcon>
+              <WifiTetheringOffIcon
+                sx={(theme) => ({ color: theme.palette.grey[300] })}
+              />
+            </ListItemIcon>
+            <ListItemText
+              slotProps={{ primary: { variant: "body2" } }}
+              primary="Goldfish"
+            />
+          </ListItem>
+          <ListItem>
+            <ListItemIcon>
+              <WifiTetheringOffIcon
+                sx={(theme) => ({ color: theme.palette.grey[300] })}
+              />
+            </ListItemIcon>
+            <ListItemText
+              slotProps={{ primary: { variant: "body2" } }}
+              primary="Mechano cat and Robot-Mouse"
+            />
+          </ListItem>
+        </List>
+      </CardContent>
+    </Card>
+  );
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,6 +4,9 @@ import { DialogDemo } from "../components/demo/Dialog";
 import Stack from "@mui/material/Stack";
 import { StepperDemo } from "../components/demo/Stepper";
 import Divider from "@mui/material/Divider";
+import Container from "@mui/material/Container";
+import { SidePanelCardDemo } from "../components/demo/SidePanelCard";
+import Box from "@mui/material/Box";
 
 export const Route = createFileRoute("/")({
   component: Index,
@@ -14,11 +17,23 @@ function Index() {
     <div>
       <h3>Welcome Home!</h3>
 
-      <Stack direction="column" spacing={2} divider={<Divider />} sx={{ p: 2 }}>
-        <IconButtonDemo />
-        <DialogDemo />
-        <StepperDemo />
-      </Stack>
+      <Container maxWidth="md">
+        <Stack
+          divider={<Divider flexItem />}
+          direction="column"
+          spacing={2}
+          sx={{ p: 2, alignItems: "center" }}
+        >
+          <IconButtonDemo />
+          <DialogDemo />
+          <Box sx={{ width: 560 }}>
+            <StepperDemo />
+          </Box>
+          <Box sx={{ width: 320 }}>
+            <SidePanelCardDemo />
+          </Box>
+        </Stack>
+      </Container>
     </div>
   );
 }
